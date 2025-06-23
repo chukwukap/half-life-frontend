f5aed5072a02b7639ceb98678da2456e354e02ab
Chukwuka Uba <promiseuba67@gmail.com>
2025-06-23 13:54:34 +0100

    update next config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["cryptologos.cc", "assets.coingecko.com"],
+    domains: ["cryptologos.cc"],
   },
 };
 
